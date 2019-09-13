2db77b0d7803410cb4c2a9a320189c073f578f9f
김승균 <bonoman77@gmail.com>
2019-09-13 22:18:22 +0900

    상단검색 프로시저 개발 완료

--- Stored Procedure/00. Common.sql
+++ Stored Procedure/00. Common.sql
@@ -29,3 +29,80 @@ WHERE i.UseYn=1
    	  AND i.Separate=@Separate
 	  AND i.ID=CASE WHEN @SelectedID IS NULL THEN i.ID ELSE @SelectedID END  
 ORDER BY i.OrderNum 
+
+
+
+
+-- =============================================
+-- Author:		김승균 
+-- Create date: 2019-08-18  
+-- Email:		bonoman77@gmail.com  
+-- Description:	대특공 상단검색 - 도시리스트 
+-- =============================================
+
+CREATE PROCEDURE [dbo].[uspGetStandingSearchMetroList]
+AS
+
+SET NOCOUNT ON;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; 
+
+SELECT 
+	m.MetroID
+	, m.MetroName 
+FROM dbo.Metros m   
+ORDER BY m.OrderNum 
+
+GO
+
+
+
+
+-- =============================================
+-- Author:		김승균 
+-- Create date: 2019-08-18  
+-- Email:		bonoman77@gmail.com  
+-- Description:	대특공 상단검색 - 지역(순회구)리스트 
+-- =============================================
+
+CREATE PROCEDURE [dbo].[uspGetStandingSearchCircuitList]
+	@MetroID int 
+AS
+
+SET NOCOUNT ON;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; 
+
+SELECT 
+	c.CircuitID
+	, c.CircuitName 
+FROM dbo.Circuits c
+WHERE c.MetroID=@MetroID    
+ORDER BY c.CircuitName
+
+GO
+
+
+
+
+
+-- =============================================
+-- Author:		김승균 
+-- Create date: 2019-08-18  
+-- Email:		bonoman77@gmail.com  
+-- Description:	대특공 상단검색 - 회중리스트 
+-- =============================================
+
+CREATE PROCEDURE [dbo].[uspGetStandingSearchCongregationList]
+	@CircuitID int 
+AS
+
+SET NOCOUNT ON;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; 
+
+SELECT 
+	g.CongregationID
+	, g.CongregationName 
+FROM dbo.Congregations g
+WHERE g.CircuitID=@CircuitID    
+ORDER BY g.CongregationName
+
+GO
